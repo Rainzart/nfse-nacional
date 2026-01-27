a0f6f18c260f193cd6c96d0483e48552f34f01e2
OswaldoAranda <oswaldo@arandaweb.com.br>
2026-01-27 00:41:54 -0300

    Inclusão Descontos NFS-e
    Adiciona suporte a vDescCondIncond no XML de NFSe

--- src/Dps.php
+++ src/Dps.php
@@ -791,6 +791,23 @@ if (isset($this->std->infdps->serv->atvevento)) {
         );
 
         //TODO Fazer grupo vDescCondIncond
+		// Grupo vDescCondIncond (dentro de <valores>)
+		$vDescIncond = $this->std->infdps->valores->vdesccondincond->vdescincond ?? null;
+		$vDescCond   = $this->std->infdps->valores->vdesccondincond->vdesccond   ?? null;
+
+		// regra: considera vazio se null, string vazia, ou "0.00" (ajuste se quiser manter 0.00)
+		$temDescIncond = ($vDescIncond !== null && $vDescIncond !== '' && $vDescIncond !== '0.00');
+		$temDescCond   = ($vDescCond   !== null && $vDescCond   !== '' && $vDescCond   !== '0.00');
+
+		if ($temDescIncond || $temDescCond) {
+			$descontos_inner = $this->dom->createElement('vDescCondIncond');
+			$valores_inner->appendChild($descontos_inner);
+
+			$this->dom->addChild($descontos_inner, 'vDescIncond', $vDescIncond, false);
+			$this->dom->addChild($descontos_inner, 'vDescCond',   $vDescCond,   false);
+		}
+		
+		
         //TODO Fazer grupo vDedRed
 
         $trib_inner = $this->dom->createElement('trib');
